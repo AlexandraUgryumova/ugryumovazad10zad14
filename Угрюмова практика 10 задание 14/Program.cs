7c44fd1ef6069eaa69784fc842616fefd1480a91
AlexandraUgryumova <124123433+AlexandraUgryumova@users.noreply.github.com>
2023-02-01 11:33:47 +0500

    initial commit

---
+++ Угрюмова практика 10 задание 14/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Угрюмова_практика_10_задание_14
+{
+    class Program
+    {
+        static int[,] Poezd_Random(int [,] array)
+        {
+            Random rnd = new Random();
+            for(int i = 0; i< array.GetLength(0); i++)
+            {
+                for(int j =0; j< array.GetLength(1); j++)
+                {
+                    array[i, j] = rnd.Next(0, 2);
+                }
+            }
+            return array;
+        }
+
+        static int[,] Poezd_Sam(int [,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    while (true)
+                    {
+                        Console.WriteLine($"Введите заполнено ли место {j + 1} в {i + 1} вагоне");
+                        array[i, j] = int.Parse(Console.ReadLine());
+                        if (array[i, j] == 1 || array[i, j] == 0) break;
+                        else Console.WriteLine("такого значения не может быть");
+                    }
+                }
+            }
+            return array;
+        }
+        static void Show(int [,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i,j] +" ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static int [] Chet(int[,] array)
+        {
+            int[] chet = new int[10];
+            for(int i = 0; i< array.GetLength(0); i++)
+            {
+                for(int j = 0; j< array.GetLength(1); j++)
+                {
+                    if(array[i,j] == 0)
+                    {
+                        chet[i]++;
+                    }
+                }
+            }
+            return chet;
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    int [,] array = new int [10, 36];
+                    Console.WriteLine("вы хотите написать самостоятельно?");
+                    string answer = Console.ReadLine();
+                    if (answer == "да") Poezd_Sam(array);
+                    else
+                    {
+                        if (answer == "нет") Poezd_Random(array);
+                    }
+                    Show(array);
+                    int[] chet = Chet(array);
+                    for(int i = 0; i< 10; i++)
+                    {
+                        Console.WriteLine($"{chet[i]} свободных мест в {i+1} вгоне");
+                    }
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("ошибка в наборе");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Угрюмова практика 10 задание 14/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Угрюмова практика 10 задание 14")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Угрюмова практика 10 задание 14")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a6c158c6-86f2-48c9-8f81-56f4e340bdfe")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
